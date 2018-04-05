f5c8f8096b792b737415186e3a63ca5e4e5a1aee
knulps <dhlee@thecheat.co.kr>
2018-04-05 17:15:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# React 16 test
+
+To master all the new features of React 16
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component, Fragment} from "react";
+
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
