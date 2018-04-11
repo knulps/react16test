598ca4352d25b5d0e720d9ea86e3e4a81e24d864
knulps <dhlee@thecheat.co.kr>
2018-04-11 18:07:17 +0900

    Return Types String and Fragments, Portals, Error Boundaries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import React, { Component, Fragment} from "react";
+import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
+
+const Message = () => "Just touched it!";
+
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
+
+const ErrorFallback = () => " Sorry something went wrong";
 
 class App extends Component {
+  state = {
+    hasError: false
+  };
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({ hasError: true });
+  };
   render() {
-    return <div className="App" />;
+    const { hasError } = this.state;
+    return (
+      <Fragment>
+        <ReturnTypes />
+        <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
+      </Fragment>
+    );
   }
 }
 
